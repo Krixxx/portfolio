091df8d186199f957c4ca7a8c4c5c8a421d83e26
Kristjan <krix@planet.ee>
2021-12-06 23:51:59 +0200

    footer icons

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 const Footer = () => {
-  return <FooterArea>Hello</FooterArea>;
+  return (
+    <FooterArea>
+      <div className='social-icons'>
+        <a
+          href='https://github.com/Krixxx'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaGithub />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/kristjan-lepik-46905211a/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+    </FooterArea>
+  );
 };
 
 const FooterArea = styled.section`
-  height: 300px;
-  background-color: var(--clr-footer-bcg);
+  margin-top: 1rem;
+  .social-icons {
+    justify-content: center;
+    margin-bottom: 1rem;
+    display: flex;
+    gap: 1rem;
+  }
+  .social-icons svg {
+    font-size: 2rem;
+    opacity: 0.5;
+  }
 `;
 
 export default Footer;
